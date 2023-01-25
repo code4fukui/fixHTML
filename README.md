a1c8d7c7795dce0f14bf87548d3dcaf57a6779f9
Taisuke Fukuno <fukuno@jig.jp>
2023-01-25 19:32:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fixHTML
+ 
---
+++ fixHTML.js
@@ -0,0 +1,33 @@
+import { esreform } from "https://code4fukui.github.io/esreform/esreform.js";
+
+export const fixHTML = (src) => {
+  const s = ZenkakuAlpha.toHan(src);
+  let n = 0;
+  const ss = [];
+  for (;;) {
+    const m = s.indexOf("<script", n);
+    if (m < 0) {
+      break;
+    }
+    const k = s.indexOf(">", m);
+    if (k < 0) {
+      break;
+    }
+    const m2 = s.indexOf("</" + "script>", k + 1);
+    if (m2 < 0) {
+      break;
+    }
+    const src = s.substring(k + 1, m2);
+    if (src.trim() == "") {
+      ss.push(s.substring(n, m2 + 9));
+    } else {
+      const src2 = src.trim() == "" ? src : esreform(src);
+      ss.push(s.substring(n, k + 1) + "\n");
+      ss.push(src2);
+      ss.push("\n</" + "script>");
+    }
+    n = m2 + 9;
+  }
+  ss.push(s.substring(n));
+  return ss.join("");
+};
